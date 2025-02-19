903a77d0f33ac8b81096a5c5f28f90ce91eabda6
nancyromales19 <nancyromales16@gmail.com>
2025-02-19 21:34:29 +0800

    Populating product.database.ts file

--- src/products/product.database.ts
+++ src/products/product.database.ts
@@ -1,114 +1,78 @@
-import { Product, Products, UnitProduct} from "./product.interface";
-import {v4 as random} from "uuid";
+import { Product, Products, UnitProduct } from "./product.interface";
+import { v4 as random } from "uuid";
 import fs from "fs";
 
-let products: Users = loadUsers()
+let products: Products = loadProducts();
 
-function loadUsers () : Users {
+function loadProducts(): Products {
     try {
-        const data = fs.readFileSync("./users.json", "utf-8")
-        return JSON.parse(data)
-    }
-    catch(error) {
-        console.log(`Error ${error}`)
-        return {}
+        const data = fs.readFileSync("./products.json", "utf-8");
+        return JSON.parse(data);
+    } catch (error) {
+        console.log(`Error ${error}`);
+        return {};
     }
 }
 
-function saveUsers () {
+function saveProducts() {
     try {
-        fs.writeFileSync("./users.json", JSON.stringify(users), "utf-8")
-        console.log(`User saved succesfully!`)
-    }
-    catch (error) {
-        console.log(`Error : ${error}`)
+        fs.writeFileSync("./products.json", JSON.stringify(products), "utf-8");
+        console.log("Products saved successfully!")
+    } catch (error) {
+        console.log("Error", error)
     }
 }
 
-export const findAll = async (): Promise<UnitUser[]> => Object.values(users);
-export const findOne = async (id: string): Promise<UnitUser> => users[id];
-export const create = async (userData: UnitUser): Promise<UnitUser | null> => {
-    let id = random()
-    let check_user = await findOne(id);
+export const findAll = async () : Promise<UnitProduct[]> => Object.values(products)
 
-    while (check_user) {
-        id = random()
-        check_user = await findOne(id)
+export const findOne = async (id : string) : Promise<UnitProduct> => products[id]
+
+export const create  = async (productInfo : Product) : Promise<null | UnitProduct> => {
+
+    let id = random ()
+
+    let product = await findOne(id)
+
+    while (product) {
+        id = random ()
+        await findOne(id)
     }
 
-    const salt = await bcrypt.genSalt(10);
-
-    const hashedPassword = await bcrypt.hash(userData.password, salt);
-
-    const user : UnitUser = {
+    products[id] = {
         id : id,
-        username : userData.username,
-        email: userData.email,
-        password: hashedPassword
-    };
-
-    users[id] = user;
-
-    saveUsers()
-
-    return user;
-};
-
-export const findByEmail = async (user_email: string): Promise<null | UnitUser> => {
-    const allUsers = await findAll();
-
-    const getUser = allUsers.find(result => user_email === result.email);
-
-    if (!getUser) {
-        return null;
+        ...productInfo
     }
 
-    return getUser;
-};
-
-export const comparePassword = async (email : string, supplied_password : string) : Promise<null | UnitUser> => {
-    const user = await findByEmail(email)
-
-    const decryptPassword = await bcrypt.compare(supplied_password, user!.password)
-
-    if(!decryptPassword) {
-        return null
-    }
-
-    return user
+    saveProducts()
+    return products[id]
 }
 
-export const update = async (id : string, updateValues : User) : Promise<UnitUser | null> => {
-    const userExists = await findOne(id)
+export const update = async (id : string, updateValues : Product) : Promise<UnitProduct | null> => {
 
-    if(!userExists) {
+    const product = await findOne(id)
+
+    if (!product) {
         return null
     }
 
-    if(updateValues.password) {
-        const salt = await bcrypt.genSalt(10)
-        const newPass = await bcrypt.hash(updateValues.password, salt)
-
-        updateValues.password = newPass
-    }
-
-    users[id] = {
-        ...userExists,
+    products[id] = {
+        id,
         ...updateValues
     }
 
-    saveUsers()
-    return users[id]
-} 
+    saveProducts()
+    return products[id]
+}
 
-export const remove = async(id : string) : Promise<null | void> => {
-    const user = await findOne(id)
+export const remove = async (id : string) : Promise<null | void> => {
 
-    if (!user) {
+    const product = await findOne(id)
+
+    if (!product) {
         return null
     }
 
-    delete users[id]
+    delete products[id]
 
-    saveUsers()
+    saveProducts()
 }
