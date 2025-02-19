9cbd19f8d119d84ec078d40164a294ef5bc269ba
nancyromales19 <nancyromales16@gmail.com>
2025-02-19 18:01:31 +0800

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express from "express"
 import dotenv from "dotenv"
 import cors from "cors"
 import helmet from "helmet"
+import { userRouter } from "./users/users.routes"
 
 
 dotenv.config()
@@ -21,6 +22,7 @@ app.use(express.urlencoded({extended : true}))
 app.use(cors())
 app.use(helmet())
 
+app.use('/', userRouter)
 
 app.listen(PORT, () =>{
     console.log(`Server is listening on port ${PORT}`)
